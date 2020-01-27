54dd5118117c70a53f10072e33755402caac7067
Mikhail Podmogov <podmogov@MacBook-Pro-Mikhail.local>
2020-01-28 01:18:40 +0300

    init

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'smartweb adapter settings': {
+        en: 'Adapter settings for smartweb',
+        de: 'Adaptereinstellungen für smartweb',
+        ru: 'Настройки адаптера для smartweb',
+        pt: 'Configurações do adaptador para smartweb',
+        nl: 'Adapterinstellingen voor smartweb',
+        fr: "Paramètres d'adaptateur pour smartweb",
+        it: "Impostazioni dell'adattatore per smartweb",
+        es: 'Ajustes del adaptador para smartweb',
+        pl: 'Ustawienia adaptera dla smartweb',
+        'zh-cn': 'smartweb的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'option2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        'zh-cn': '选项2',
+    },
+};
