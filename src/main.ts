faa847affda953f1bde7cb7a41996cc71f2810d3
Mikhail Podmogov <podmogov@MacBook-Pro-Mikhail.local>
2020-02-14 01:44:59 +0300

    Fix

--- src/main.ts
+++ src/main.ts
@@ -82,23 +82,19 @@ class Smartweb extends utils.Adapter {
             .then(async () => {
                 this.setStateAsync('info.connection', true);
                 this.log.info('Connected, wait fot read.');
-
-                if (client.isOpen) {
-                    await client
-                        .readHoldingRegisters(40145, 1)
-                        .then(async data => {
-                            await this.setStateAsync('testVariable', { val: data.data[0], ack: true });
-                        })
-                        .catch(function(e) {
-                            throw e;
-                        });
-                }
+                this.log.info('isOpen = ' + client.isOpen);
+                await client
+                    .readHoldingRegisters(40145, 1)
+                    .then(async data => {
+                        await this.setStateAsync('testVariable', { val: data.data[0], ack: true });
+                    })
+                    .catch(function(e) {
+                        throw e;
+                    });
             })
             .catch(function(e) {
                 throw e;
             });
-
-        this.log.info('isOpen = ' + client.isOpen);
 
         // modbus.tcp.connect(502, '192.168.88.31', { debug: 'automaton-2454' }, async (err, connection) => {
         //     if (err) {
